bd8a87bae712761b801a260f1cbe2bef9583c700
Adam Ou-Yang <adon988@users.noreply.github.com>
2019-07-12 00:47:19 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -72,7 +72,7 @@ service postfix restart
 # go
 cd /
 wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
-tar -C /bin -xzf go1.12.6.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.12.6.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 rm -rf go1.12.6.linux-amd64.tar.gz
 
