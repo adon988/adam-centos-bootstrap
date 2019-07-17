b54a140da69cadf6ca599315767e90270457e591
Adam Ou-Yang <adon988@users.noreply.github.com>
2019-07-17 22:48:06 +0800

    Create boostrap-go.sh

---
+++ boostrap-go.sh
@@ -0,0 +1,13 @@
+#!bin/shell
+
+
+# Install Go
+wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.12.6.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+rm -rf go1.12.6.linux-amd64.tar.gz
+
+# Go vim
+# https://github.com/fatih/vim-go
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
