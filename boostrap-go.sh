2367a578207f08f045979e2dcec137bfa67be01b
Adam Ou-Yang <adon988@users.noreply.github.com>
2019-07-17 23:06:36 +0800

    Update boostrap-go.sh

--- boostrap-go.sh
+++ boostrap-go.sh
@@ -5,6 +5,7 @@
 wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.12.6.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 go version
 rm -rf go1.12.6.linux-amd64.tar.gz
 
@@ -14,4 +15,4 @@ git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
 
 echo "let g:go_version_warning = 0" >> ~/.vimrc
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+
